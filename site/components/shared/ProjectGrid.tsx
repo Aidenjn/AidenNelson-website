c82ae83d6509e832d84bec1893ccb3c58f68e6df
Aiden Nelson <aidenjn@gmail.com>
2026-01-12 14:06:22 -0800

    Page under construction notice.

--- site/components/shared/ProjectGrid.tsx
+++ site/components/shared/ProjectGrid.tsx
@@ -1,12 +1,13 @@
 import { Artwork } from '@/lib/types/SanityTypes';
-import ArtworkCard from './ArtworkCard';
+import PageHeading from './PageHeading';
 
 export default function ProjectGrid({ projects }: { projects: Artwork[] }) {
   return (
-    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-8">
-      {projects.map((artwork) => (
-        <ArtworkCard key={artwork._id} artwork={artwork} />
-      ))}
-    </div>
+    <PageHeading titleText='This page is under construction.  🏗️'/>
+    // <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-8">
+    //   {projects.map((artwork) => (
+    //     <ArtworkCard key={artwork._id} artwork={artwork} />
+    //   ))}
+    // </div>
   );
 }
