5f05c180c0518d3b602ffab3494b20ffc5349585
Aiden Nelson <aidenjn@gmail.com>
2026-01-12 14:08:27 -0800

    Prettier formatting.

--- site/components/shared/ProjectGrid.tsx
+++ site/components/shared/ProjectGrid.tsx
@@ -3,7 +3,7 @@ import PageHeading from './PageHeading';
 
 export default function ProjectGrid({ projects }: { projects: Artwork[] }) {
   return (
-    <PageHeading titleText='This page is under construction.  🏗️'/>
+    <PageHeading titleText="This page is under construction.  🏗️" />
     // <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-8">
     //   {projects.map((artwork) => (
     //     <ArtworkCard key={artwork._id} artwork={artwork} />
