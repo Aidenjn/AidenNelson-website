64671f3a8216defa36ca7380c3720f85a2f19f41
Aiden Nelson <aidenjn@gmail.com>
2026-03-05 11:35:45 -0800

    Card banner looks a little nicer.

--- site/components/shared/BannerOverlay.tsx
+++ site/components/shared/BannerOverlay.tsx
@@ -1,25 +1,24 @@
 'use client';
 
 const BannerOverlay = ({ title }: { title: string }) => (
-  <div className="p-5 w-full flex align-middle justify-around">
+  <div className="px-4.5 py-5 w-full flex align-middle justify-around">
     <div
       className="
       w-full
-      absolute
+      h-full
       border-6
-      border-main-accent
-      bg-main-accent
+      border-secondary-accent
+      bg-secondary-accent
       z-30
       text-center
       text-lg
       text-foreground
       rounded-sm
-      origin
-      -translate-y-[90%]
+      -mt-34
     "
     >
       <div className="border-6 border-background rounded-sm bg-background">
-        <div className="border-7 border-foreground rounded-sm p-4 bg-background text-md">
+        <div className="border-7 border-foreground rounded-sm p-4 bg-background text-md nav-link-in-content">
           {title}
         </div>
       </div>
