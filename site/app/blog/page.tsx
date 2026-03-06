31e0dea53877ad0c4d5c9563de5a3a93397773dd
Aiden Nelson <aidenjn@gmail.com>
2026-03-06 10:26:59 -0800

    Added icon to blog page under construction.

--- site/app/blog/page.tsx
+++ site/app/blog/page.tsx
@@ -1,11 +1,15 @@
 // import { client } from '@/lib/sanity';
 
 import PageHeading from '@/components/shared/PageHeading';
+import { FaHammer } from 'react-icons/fa';
 
 export default async function BlogPage() {
   return (
     <div>
-      <PageHeading titleText="Under construction 🏗️" />
+      <PageHeading titleText="Page under construction..." />
+      <div className="w-full text-center pt-15">
+        <FaHammer className="inline text-8xl text-foreground" />
+      </div>
     </div>
   );
 }
